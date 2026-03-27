6f5cd7270d56706c364521fb38e90556f278f49f
ahmad <ahmedorfaly1982@gmail.com>
2026-03-27 12:02:33 +0000

    improve presentation by adding imojis and breaking lines

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 """Rock, Paper, Scissors game """
 import random
-options = ["r", "p", "s"]
-wining_comp = [("r", "s"), ("p", "r"), ("s", "p")]
+options = ("r", "p", "s")
+wining_comp = (("r", "s"), ("p", "r"), ("s", "p"))
+emojis = {"r":"🪨", "p":"📰", "s":"✂️"}
 
 def print_choces():
-    print(f"you chose: {user_choice}, Computer chose: {computer_choice}")
+    print("----------------------------")
+    print(f"you chose: {emojis[user_choice]}")
+    print(f"Computer chose: {emojis[computer_choice]}")
+    print("----------------------------")
 
 while True:
     user_choice = input("Rock, Paper or scissors (R/P/S) / Q to quit: ")
@@ -14,13 +18,21 @@ while True:
         break
     elif user_choice.lower() not in options:
         print("Enter a valid option (R, P, or S)")
+        print("_____________________________________")
+        print()
         continue
     elif user_choice == computer_choice:
         print_choces()
         print("It's a tie!")
+        print("_____________________________________")
+        print()
     elif (user_choice, computer_choice) in wining_comp:
         print_choces()
         print("You won!")
+        print("_____________________________________")
+        print()
     else:
         print_choces()
         print("Computer won!")
+        print("_____________________________________")
+        print()
