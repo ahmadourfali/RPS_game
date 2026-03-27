c52223518ffc9cfb31d2e96710e564bf69f3417e
ahmad <ahmedorfaly1982@gmail.com>
2026-03-27 08:11:05 +0000

    add random selection logic to main script

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 """Rock, Paper, Scissors game """
+import random
+options = ["Rock", "Paper", "Scissors"]
+
+user_choice = input("Rock, Paper or scissors (R/P/S):")
+computer_choice = random.choice(options)
