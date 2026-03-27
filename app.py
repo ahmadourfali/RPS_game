d866126695189f7ba24c31a5c83aafa9c4234562
ahmad <ahmedorfaly1982@gmail.com>
2026-03-27 08:49:44 +0000

    validate user input and add the logic for a tie

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 """Rock, Paper, Scissors game """
 import random
-options = ["Rock", "Paper", "Scissors"]
+options = ["r", "p", "s"]
 
-user_choice = input("Rock, Paper or scissors (R/P/S):")
-computer_choice = random.choice(options)
+
+
+while True:
+    user_choice = input("Rock, Paper or scissors (R/P/S):")
+    computer_choice = random.choice(options)
+
+    if user_choice.lower() not in options:
+        print("Enter a valid option (R, P, or S)")
+        
+    elif user_choice == computer_choice:
+        print("It's a tie!")
+        break
+    else:
+        print(f"you chose: {user_choice}, Computer chose: {computer_choice}")
